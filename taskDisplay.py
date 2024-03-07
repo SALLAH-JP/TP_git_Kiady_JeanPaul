a120aabb2b08c6cdec0aabb08057116cabef4c30
SALLAH-JP <jeanpaulsallah@gmail.com>
2024-03-07 14:14:19 +0400

    creating display file

--- taskDisplay.py
+++ taskDisplay.py
@@ -1,2 +1,12 @@
+import sys
+sys.path.append('manageTask/taskTool.py')
+
+import taskTool
+
 Choix = int(input("Que voulez vous faire ? \nEntrer O si vous voulez charger une liste de taches ou 1 si vous voulez créer une liste de taches : "))
 
+Dico = {}
+
+taskTool.AddTache(Dico, "nom")
+
+print(Dico)
