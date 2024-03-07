cf2f0d1120adbdc0cc9050e311fe36d11893dadb
SALLAH-JP <jeanpaulsallah@gmail.com>
2024-03-07 15:17:57 +0400

    Version 5 de taskDisplay

--- taskDisplay.py
+++ taskDisplay.py
@@ -14,7 +14,7 @@ if Choix == 1:
 elif Choix == 0:
     Dico = dataTask.Load()
 
-Choix = int(input("Voulez vous sauvegarder ?/nEntrer O pour "Oui" et 1 pour "Non""))
+Choix = int(input("Voulez vous sauvegarder ?\nEntrer O pour Oui et 1 pour Non : "))
 
 if Choix == 0:
     Nom = input("Quel est le nom de votre fichier ? : ")
