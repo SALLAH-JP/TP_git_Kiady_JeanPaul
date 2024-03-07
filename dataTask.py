129754bcc9b621be16fc34501a52ceeb46077c26
Kiady <kiadymrabenarivo@gmail.com>
2024-03-07 12:38:03 +0400

    Ajout de la fonction qui supprime la base de donnees

--- dataTask.py
+++ dataTask.py
@@ -12,5 +12,10 @@ def Load ( file_name):
 	data = json.load(json_file)
     return data
 
-		
+import os
+
+def delete_database(filename):
+	
+    if os.path.exits(filename):
+        os.remove(filename)			
     	
