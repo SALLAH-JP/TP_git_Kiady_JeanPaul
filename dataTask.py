bad55f63550f242601717d39834adf45d2563311
SALLAH-JP <jeanpaulsallah@gmail.com>
2024-03-07 14:58:59 +0400

    Modif DataTask

--- dataTask.py
+++ dataTask.py
@@ -3,13 +3,13 @@ import os
 def SaveDic (dictionary,  file_name):
 
     with open(file_name, "w") as fichier:
-	json.dump(dictionary , fichier )	
+        json.dump(dictionary , fichier )	
 
 
 def Load ():
     filename = input("Quel est le nom de la liste que vous voulez generer: ")
     with open(filename, 'r') as json_files:
-	data = json.load(json_file)
+        data = json.load(json_file)
     return data
 
 
