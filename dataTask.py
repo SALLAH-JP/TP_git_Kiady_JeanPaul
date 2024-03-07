07958df3365b96ec6fded5e92095e374dc4ecbe1
kiki781227 <161693476+kiki781227@users.noreply.github.com>
2024-03-07 14:36:34 +0400

    Update dataTask.py
    Ajout d'un input()

--- dataTask.py
+++ dataTask.py
@@ -6,9 +6,9 @@ def SaveDic (dictionary,  file_name):
 	json.dump(dictionary , fichier )	
 
 
-def Load ( file_name):
-    
-    with open("filename, 'r') as json_files:
+def Load ():
+    filename = input("Quel est le nom de la liste que vous voulez generer: ")
+    with open(filename, 'r') as json_files:
 	data = json.load(json_file)
     return data
 
